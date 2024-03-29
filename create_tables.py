b53ddd4775c762df620642d773f6d0f17aa88872
Huyen-Phan <72473316+Huyen-P@users.noreply.github.com>
2024-03-29 15:36:10 +0100

    update
    update

--- create_tables.py
+++ create_tables.py
@@ -1,6 +1,6 @@
 import configparser
 import psycopg2
-from sql_queries import create_table_queries, drop_table_queries
+from sql_queries2 import create_table_queries, drop_table_queries
 
 
 def drop_tables(cur, conn):
@@ -17,7 +17,7 @@ def create_tables(cur, conn):
 
 def main():
     config = configparser.ConfigParser()
-    config.read('dwh.cfg')
+    config.read('dwhhuyen.cfg')
 
     conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config['CLUSTER'].values()))
     cur = conn.cursor()
